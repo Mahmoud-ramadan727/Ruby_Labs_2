ad9bb9d995e0d592cb4c2df564b26071ec89a15a
Mahmoud-ramadan727 <mahmoud.ramdan1994@gmail.com>
2020-04-27 16:16:45 +0200

    use devise for auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
 
-    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
-    
+ 
+    before_action :authenticate_user!
+ 
     def new
         @article = Article.new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # devise_for :users, controllers: { sessions: 'users/sessions' }   
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+   
+  devise_for :users, skip: :all
+
   get 'welcome/index'
 
   resources :articles
@@ -7,5 +12,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'welcome#index'
+  root to: 'welcome#index'
+  # root to: 'home#index'
  end
